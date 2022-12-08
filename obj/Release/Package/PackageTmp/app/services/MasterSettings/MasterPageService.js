220518a3103027da716ae40d4c2e0d9bc2ec07c5
Peddirsi Viswanth <viswanthvishu1000@gmail.com>
2022-12-08 19:16:14 +0530

    commit all

---
+++ obj/Release/Package/PackageTmp/app/services/MasterSettings/MasterPageService.js
@@ -0,0 +1,12 @@
+﻿define(['app'], function (app) {
+    app.service("MasterPageService", function (DataAccessService) {
+
+        //this.AddModule = function (ModuleName, ModuleRouteName, ModuleCardColourID, UserName) {
+        //    var paramObj = {
+        //        "ModuleName": ModuleName, "ModuleRouteName": ModuleRouteName, "ModuleCardColourID": ModuleCardColourID, "UserName": UserName
+        //    };
+        //    var promise = DataAccessService.getDataWithPara('api/MasterPageService/AddModule', paramObj);
+        //    return promise;
+        //}
+    });
+});
